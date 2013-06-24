6951042507e98ce79231db047fcf339e25bc5b88
rhannequin <remy.hannequin@gmail.com>
2013-06-24 12:01:25 +0200

    Begin tldr

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,13 +1,4 @@
 define(['jquery', 'lodash', 'backbone'], function ($, _, Backbone) {
   'use strict';
 
-  require(['text!json.json'], function (json) {
-    json = JSON.parse(json);
-    var template = _.template($('#repo-list-template').text());
-    var $main = $('.container');
-    $main.append(template({ repos: json }));
-  });
-
-  // require(['generate-json']);
-
 });
